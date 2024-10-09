81ca6ac6dae6d305d3650a32438bccf75ae87c31
mia <mhallmark14@gmail.com>
2024-10-09 14:45:46 -0700

    Create initials.py

---
+++ hello-world/initials.py
@@ -0,0 +1,9 @@
+# lmao i think i could have just made some skinny letters
+
+# i really like strawberries!
+print('MMMM          MMMM  HH            HH')
+print('MMMM M MMMM M MMMM  HH            HH')
+print('MMMM  MMMMMM  MMMM  HH            HH')
+print('MMMM          MMMM  HH HHHHHHHHHH HH')
+print('MMMM          MMMM  HH            HH')
+print('MMMM          MMMM  HH            HH') 
