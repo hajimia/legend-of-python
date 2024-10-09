4e76a08a1fcb0756ae613c1ea75e9914631ded66
mia <mhallmark14@gmail.com>
2024-10-09 14:46:15 -0700

    Create letter.py

---
+++ hello-world/letter.py
@@ -0,0 +1,6 @@
+# letter.py program!
+print('10/09/2024')
+print('I am feeling really "meh". I feel like there are a lot of things I want to do but not enough time to do them!')
+print('I hope to establish foundations by learning how to code so I can streamline processes I do in my every day life/workplace')
+print('I hope you have fun coding! make it fun! it is not a chore!')
+print('💙')
