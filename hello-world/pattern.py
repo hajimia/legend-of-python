f94ad3e0639ee9d35cd2c63517040255516656e2
mia <mhallmark14@gmail.com>
2024-10-09 14:44:54 -0700

    Create pattern.py

---
+++ hello-world/pattern.py
@@ -0,0 +1,7 @@
+# we are making a pattern!
+# initially i was doing this totally wrong bc i thought way too hard but i simplified it :3
+
+print('1')
+print('2 3')
+print('4 5 6')
+print('7 8 9 10')
