56bb7a49dc19a4c08a3755c012cc234e7f655596
mia <mhallmark14@gmail.com>
2024-10-11 16:22:23 -0700

    Create 99_bottles.py

---
+++ loops/99_bottles.py
@@ -0,0 +1,6 @@
+# string interpolation
+for x in range(99, 0, -1):
+    print(f'{x} bottles of beer on the wall')
+    print(f'{x} bottles of beer')
+    print('Take one down, pass it around')
+    print(f'{x-1} bottles of beer on the wall')
